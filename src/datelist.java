3901f81ecba1453f67dd243228cab3a353ca09f1
dyrongrath <118616647+Dy-Rongrath@users.noreply.github.com>
2023-05-26 00:15:02 +0700

    Add datelist.java

---
+++ src/datelist.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+
+public class datelist {
+    private Date date;
+
+    public datelist(Date date){
+        this.date= date;
+    }
+    public Date getDate(){
+        return date;
+    }
+
+}
