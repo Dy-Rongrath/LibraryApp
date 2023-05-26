aecd9af054f6b43427b1af3bcc51b7fa1f2df9c8
DariyaDourng <126034461+DariyaDourng@users.noreply.github.com>
2023-05-26 23:00:52 +0700

    Commit for booklist

---
+++ src/booklist.java
@@ -0,0 +1,31 @@
+
+public class booklist {
+    private int bookid;
+    private String bookname;
+    private String authorname;
+    private String subject;
+    private String status;
+
+    public booklist(int bookid, String bookname, String authorname, String subject, String status){
+        this.bookid = bookid;
+        this.bookname = bookname;
+        this.authorname = authorname;
+        this.subject = subject;
+        this.status = status;
+    }
+    public int getBookid(){
+        return bookid;
+    }
+    public String getBookname(){
+        return bookname;
+    }
+    public String getAuthorname(){
+        return authorname;
+    }
+    public String getSubject(){
+        return subject;
+    }
+    public String getStatus(){
+        return status;
+    }
+}
