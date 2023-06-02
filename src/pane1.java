a6cadb756adab68af5afb81e58298c392ac74d14
Mony-Ratanak <hieklymonyratanak@gmail.com>
2023-06-02 15:13:34 +0700

    add_pane1.java

---
+++ src/pane1.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class pane1 implements Initializable{
+    @FXML
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+    
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    @FXML
+    private void home(MouseEvent event){
+        bp.setCenter(ap);
+    }
+    @FXML
+    private void page01(MouseEvent event) throws IOException{
+        App a = new App();
+        a.changeScene("checkout.fxml");
+    }
+    @FXML
+    private void page02(MouseEvent event) throws IOException{
+        App a = new App();
+        a.changeScene("checkin.fxml");
+    }
+    @FXML
+    private void page03(MouseEvent event) throws IOException{
+        App a = new App();
+        a.changeScene("onhold.fxml");
+    }
+    @FXML
+    private void page04(MouseEvent event) throws IOException{
+        App a = new App();
+        a.changeScene("addbook.fxml");
+    }
+    
+}
